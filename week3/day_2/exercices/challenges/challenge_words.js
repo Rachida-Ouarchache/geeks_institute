4b2578f8a35d35227150cde9df2772bf61672280
rachida-asic <rachidaou49@gmail.com>
2025-09-07 21:56:55 +0100

    Challenge Words in the stars Day2 Week3

--- week3/day_2/exercices/challenges/challenge_words.js
+++ week3/day_2/exercices/challenges/challenge_words.js
@@ -1,16 +1,22 @@
-let input = prompt("Enter several words, separated by commas:");
+const prompt = require('prompt-sync')({sigint: true});
 
-let words = input.split(",").map(word => word.trim());
+let input = prompt("Enter several words, separated by commas: ");
+
+if (!input.trim()) {
+    console.log("You didn't enter any words!");
+    process.exit(0);
+}
+
+let words = input.split(",").map(word => word.trim()).filter(word => word.length > 0);
 
 let maxLength = words.reduce((max, word) => Math.max(max, word.length), 0);
 
-let border = "*".repeat(maxLength + 4); 
+let border = "*".repeat(maxLength + 4);
 
 console.log(border);
-
 words.forEach(word => {
-    let padding = " ".repeat(maxLength - word.length); 
+    let padding = " ".repeat(maxLength - word.length);
     console.log(`* ${word}${padding} *`);
 });
+console.log(border);
 
-console.log(border); 
