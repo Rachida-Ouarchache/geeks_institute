53da248bec0021a9953a2c0ddee60d4b523ac8a3
rachida-asic <rachidaou49@gmail.com>
2025-09-14 12:00:48 +0100

    Exercises Ninja Day3 Week4

---
+++ week4/day3/exercices/exercice_ninja.js
@@ -0,0 +1,17 @@
+class Bird {
+  constructor() {
+    console.log("I'm a bird. 🦢");
+  }
+}
+
+class Flamingo extends Bird {
+  constructor() {
+    console.log("I'm pink. 🌸");
+    super();
+  }
+}
+
+const pet = new Flamingo();
+
+//ReferenceError: Must call super constructor in derived class before accessing 'this' or returning from derived constructor
+
