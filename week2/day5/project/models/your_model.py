c3b4fce39957cc3bcb6eb1b36956d8af1c66b0a5
rachida-asic <rachidaou49@gmail.com>
2025-09-16 18:56:29 +0100

    remove

--- week2/day5/project/models/your_model.py
+++ week2/day5/project/models/your_model.py
@@ -2,7 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# Catégorie de plats (ex : Pizza, Dessert, Boisson)
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False, unique=True)
@@ -10,14 +9,12 @@ class Category(db.Model):
     menu_items = db.relationship('MenuItem', backref='category', lazy=True)
 
 
-# Chef (personne qui prépare les plats)
 class Chef(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     specialty = db.Column(db.String(100))
 
 
-# Plat (élément du menu)
 class MenuItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
@@ -27,7 +24,6 @@ class MenuItem(db.Model):
     chef_id = db.Column(db.Integer, db.ForeignKey('chef.id'), nullable=True)
 
 
-# Commande (client qui commande plusieurs plats)
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     customer_name = db.Column(db.String(100), nullable=False)
