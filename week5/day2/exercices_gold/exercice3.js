bccc35fa99b0215291f0ca480ccd05fc8f229d18
rachida-asic <rachidaou49@gmail.com>
2025-09-22 11:13:53 +0100

    Exercises Gold Day2 Week5

---
+++ week5/day2/exercices_gold/exercice3.js
@@ -0,0 +1,40 @@
+let resolveAfter2Seconds = function () {
+    console.log("starting slow promise");
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve("slow");
+            console.log("slow promise is done");
+        }, 2000);
+    });
+};
+
+let resolveAfter1Second = function () {
+    console.log("starting fast promise");
+    return new Promise(resolve => {
+        setTimeout(function () {
+            resolve("fast");
+            console.log("fast promise is done");
+        }, 1000);
+    });
+};
+
+let concurrentStart = async function () {
+    console.log('==CONCURRENT START with await==');
+    const slow = resolveAfter2Seconds();
+    const fast = resolveAfter1Second();
+    console.log(await slow);
+    console.log(await fast);
+}
+
+setTimeout(concurrentStart, 4000)
+// output:
+// ==CONCURRENT START with await==
+// starting slow promise
+// starting fast promise
+// (wait 1 second)
+// fast promise is done 
+// fast
+// (wait 1 more second)
+// slow promise is done
+// slow
+// Total time: ~2 seconds
